3e4028bdd76299d9ac3bc8323530f6b2ab03c586
Swetony <swetonyancelmo@gmail.com>
2025-10-31 19:50:18 -0300

    chore: Inicia e configura projeto

---
+++ src/main/java/com/sistema_hospitalar/ApiSistemaHospitalarApplication.java
@@ -0,0 +1,13 @@
+package com.sistema_hospitalar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiSistemaHospitalarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiSistemaHospitalarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sistema_hospitalar/ApiSistemaHospitalarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sistema_hospitalar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiSistemaHospitalarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
