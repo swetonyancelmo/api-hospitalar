337a59ad5736c3dfa0aae16db19aed3b715739b6
Swetony <swetonyancelmo@gmail.com>
2025-11-01 10:38:24 -0300

    feat: Adiciona configuração do Swagger

---
+++ src/main/java/com/sistema_hospitalar/config/SwaggerConfig.java
@@ -0,0 +1,32 @@
+package com.sistema_hospitalar.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI(){
+        return new OpenAPI()
+                .info(new Info()
+                        .title("API Hospital - Teste Técnico Marques Consult")
+                        .version("1.0")
+                        .description("API REST para gerenciamento de fluxo de atendimento hospitalar.")
+                        .license(new License().name("Apache 2.0").url("http://springdoc.org")))
+                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
+                .components(new Components()
+                        .addSecuritySchemes("bearerAuth", new SecurityScheme()
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")
+                                .in(SecurityScheme.In.HEADER)
+                                .name("Authorization")));
+    }
+}
