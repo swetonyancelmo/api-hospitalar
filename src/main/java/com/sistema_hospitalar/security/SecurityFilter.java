3cf68e2ff15e8957e6ee490302f429305b2164cb
Swetony <swetonyancelmo@gmail.com>
2025-10-31 21:15:33 -0300

    feat: Adiciona SecurityFilter para autenticacao via JWT

---
+++ src/main/java/com/sistema_hospitalar/security/SecurityFilter.java
@@ -0,0 +1,49 @@
+package com.sistema_hospitalar.security;
+
+import com.sistema_hospitalar.repository.UsuarioRepository;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class SecurityFilter extends OncePerRequestFilter {
+
+    @Autowired
+    private TokenService tokenService;
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
+                                    FilterChain filterChain) throws ServletException, IOException {
+        var token = this.recoverToken(request);
+
+        if(token != null){
+            var username = tokenService.validateToken(token);
+            UserDetails user = usuarioRepository.findByUsername(username);
+
+            if(user != null){
+                var authetication = new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
+                SecurityContextHolder.getContext().setAuthentication(authetication);
+            }
+        }
+        filterChain.doFilter(request, response);
+    }
+
+    private String recoverToken(HttpServletRequest request){
+        var authHeader = request.getHeader("Authorization");
+        if(authHeader == null || !authHeader.startsWith("Bearer ")) {
+            return null;
+        }
+        return authHeader.replace("Bearer ", "");
+    }
+}
