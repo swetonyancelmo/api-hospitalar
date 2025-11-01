2c102dea1667d534d657ea54103af7c19410c1b1
Swetony <swetonyancelmo@gmail.com>
2025-11-01 10:39:19 -0300

    feat: Adiciona enum para Status de Atendimento

---
+++ src/main/java/com/sistema_hospitalar/domain/enums/StatusAtendimento.java
@@ -0,0 +1,11 @@
+package com.sistema_hospitalar.domain.enums;
+
+public enum StatusAtendimento {
+    AGUARDANDO_TRIAGEM,
+    AGUARDANDO_MEDICO,
+    EM_ATENDIMENTO_MEDICO,
+    EM_MEDICACAO,
+    AGUARDANDO_REAVALIACAO,
+    ALTA,
+    FINALIZADO;
+}
