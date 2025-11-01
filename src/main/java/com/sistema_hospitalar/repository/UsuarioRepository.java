5acc4bf95898b7f45bdcb47691c2e604c77a3105
Swetony <swetonyancelmo@gmail.com>
2025-10-31 21:08:16 -0300

    feat: Adiciona repository para usuario

---
+++ src/main/java/com/sistema_hospitalar/repository/UsuarioRepository.java
@@ -0,0 +1,9 @@
+package com.sistema_hospitalar.repository;
+
+import com.sistema_hospitalar.domain.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, String> {
+    UserDetails findByUsername(String username);
+}
