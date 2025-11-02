6500b9c6e968340091f529eff1eeff7616019233
Swetony <swetonyancelmo@gmail.com>
2025-11-02 20:31:37 -0300

    feat: Adiciona método findByStatusOrderByPrioridade

--- src/main/java/com/sistema_hospitalar/repository/FichaAtendimentoRepository.java
+++ src/main/java/com/sistema_hospitalar/repository/FichaAtendimentoRepository.java
@@ -3,6 +3,7 @@ package com.sistema_hospitalar.repository;
 import com.sistema_hospitalar.domain.FichaAtendimento;
 import com.sistema_hospitalar.domain.enums.StatusAtendimento;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 import java.util.Optional;
@@ -12,4 +13,7 @@ public interface FichaAtendimentoRepository extends JpaRepository<FichaAtendimen
     Optional<FichaAtendimento> findByPacienteIdAndAtivaTrue(String pacienteId);
 
     List<FichaAtendimento> findByStatusAndAtivaTrue(StatusAtendimento status);
+
+    @Query("SELECT f FROM FichaAtendimento f JOIN f.triagem t WHERE f.status = :status AND f.ativa = true ORDER BY t.prioridade ASC")
+    List<FichaAtendimento> findByStatusOrderByPrioridade(StatusAtendimento status);
 }
