2107661c9ff81f94115c577ae8a12347c32aecd9
Swetony <swetonyancelmo@gmail.com>
2025-11-02 20:33:06 -0300

    feat: Adiciona métodos listarAguardandoMedico e registrarAvaliacaoMedica

--- src/main/java/com/sistema_hospitalar/service/AtendimentoService.java
+++ src/main/java/com/sistema_hospitalar/service/AtendimentoService.java
@@ -1,16 +1,11 @@
 package com.sistema_hospitalar.service;
 
-import com.sistema_hospitalar.domain.FichaAtendimento;
-import com.sistema_hospitalar.domain.Paciente;
-import com.sistema_hospitalar.domain.Triagem;
-import com.sistema_hospitalar.domain.Usuario;
+import com.sistema_hospitalar.domain.*;
 import com.sistema_hospitalar.domain.enums.StatusAtendimento;
-import com.sistema_hospitalar.dto.FichaAtendimentoDTO;
-import com.sistema_hospitalar.dto.IniciarAtendimentoRequestDTO;
-import com.sistema_hospitalar.dto.TriagemResponseDTO;
-import com.sistema_hospitalar.dto.TriagemResquestDTO;
+import com.sistema_hospitalar.dto.*;
 import com.sistema_hospitalar.exception.BusinessRuleException;
 import com.sistema_hospitalar.exception.ResourceNotFoundException;
+import com.sistema_hospitalar.repository.AtendimentoMedicoRepository;
 import com.sistema_hospitalar.repository.FichaAtendimentoRepository;
 import com.sistema_hospitalar.repository.PacienteRepository;
 import com.sistema_hospitalar.repository.TriagemRepository;
@@ -35,6 +30,9 @@ public class AtendimentoService {
 
     @Autowired
     private TriagemRepository triagemRepository;
+
+    @Autowired
+    private AtendimentoMedicoRepository atendimentoMedicoRepository;
 
     @Transactional
     public FichaAtendimentoDTO iniciarAtendimento(IniciarAtendimentoRequestDTO dto){
@@ -83,5 +81,57 @@ public class AtendimentoService {
         triagemRepository.save(novaTriagem);
 
         return new TriagemResponseDTO(novaTriagem);
+    }
+
+    @Transactional(readOnly = true)
+    public List<AtendimentoMedicoDTO.FilaMedico> listarAguardandoMedico(){
+        List<FichaAtendimento> fichas = fichaRepository
+                .findByStatusOrderByPrioridade(StatusAtendimento.AGUARDANDO_MEDICO);
+
+        return fichas.stream()
+                .map(AtendimentoMedicoDTO.FilaMedico::new)
+                .collect(Collectors.toList());
+    }
+
+    public AtendimentoMedicoDTO.Response registrarAvaliacaoMedica(String fichaId, AtendimentoMedicoDTO.Request dto){
+        Usuario medico= (Usuario) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+
+        FichaAtendimento ficha = fichaRepository.findById(fichaId)
+                .orElseThrow(() -> new ResourceNotFoundException("Ficha de atendimento não encontrada: " + fichaId));
+
+        if(ficha.getStatus() != StatusAtendimento.AGUARDANDO_MEDICO){
+            throw new BusinessRuleException("Esta ficha não está aguardando atendimento médico. Status atual: " + ficha.getStatus());
+        }
+
+        AtendimentoMedico atendimento = new AtendimentoMedico();
+        atendimento.setFicha(ficha);
+        atendimento.setMedico(medico);
+        atendimento.setParecer(dto.parecer());
+        atendimento.setConduta(dto.conduta());
+
+        switch (dto.conduta()) {
+            case MEDICACAO:
+                if(dto.prescricoes() == null || dto.prescricoes().isEmpty()){
+                    throw new BusinessRuleException("Conduta de MEDICAÇÃO exige ao menos uma prescrição");
+                }
+
+                List<Prescricao> prescricoes = dto.prescricoes().stream().map(pDto -> new Prescricao(null,
+                        atendimento, pDto.medicamento(), pDto.dosagem(), false)
+                ).collect(Collectors.toList());
+
+                atendimento.setPrescricoes(prescricoes);
+                ficha.setStatus(StatusAtendimento.EM_MEDICACAO);
+                break;
+
+            case ALTA:
+            case ENCAMINHAMENTO:
+                ficha.setStatus(StatusAtendimento.ALTA);
+                break;
+        }
+
+        atendimentoMedicoRepository.save(atendimento);
+        fichaRepository.save(ficha);
+
+        return new AtendimentoMedicoDTO.Response(atendimento);
     }
 }
