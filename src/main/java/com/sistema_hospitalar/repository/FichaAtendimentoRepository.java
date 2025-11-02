8d54b005f6fd6e9178ff2dff5e974cd914d8eb5d
Swetony <swetonyancelmo@gmail.com>
2025-11-02 15:41:16 -0300

    feat: Adiciona método findByStatusAndAtivaTrue

--- src/main/java/com/sistema_hospitalar/repository/FichaAtendimentoRepository.java
+++ src/main/java/com/sistema_hospitalar/repository/FichaAtendimentoRepository.java
@@ -1,11 +1,15 @@
 package com.sistema_hospitalar.repository;
 
 import com.sistema_hospitalar.domain.FichaAtendimento;
+import com.sistema_hospitalar.domain.enums.StatusAtendimento;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface FichaAtendimentoRepository extends JpaRepository<FichaAtendimento, String> {
 
     Optional<FichaAtendimento> findByPacienteIdAndAtivaTrue(String pacienteId);
+
+    List<FichaAtendimento> findByStatusAndAtivaTrue(StatusAtendimento status);
 }
