78af2cfa7f9681b8362b73cbd040b21a4a9f85bf
Swetony <swetonyancelmo@gmail.com>
2025-11-03 14:00:04 -0300

    refactor: Adiciona método findByAtendimentoMedicoIdAndAdministradoFalse

--- src/main/java/com/sistema_hospitalar/repository/PrescricaoRepository.java
+++ src/main/java/com/sistema_hospitalar/repository/PrescricaoRepository.java
@@ -3,5 +3,9 @@ package com.sistema_hospitalar.repository;
 import com.sistema_hospitalar.domain.Prescricao;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PrescricaoRepository extends JpaRepository<Prescricao, String> {
+
+    List<Prescricao> findByAtendimentoMedicoIdAndAdministradoFalse(String atendimentoMedicoId);
 }
