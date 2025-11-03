113d31777cef6bb349448afa8325ea3c1cd20cce
Swetony <swetonyancelmo@gmail.com>
2025-11-03 13:59:28 -0300

    refactor: melhora a consulta de FichaAtendimento

--- src/main/java/com/sistema_hospitalar/repository/FichaAtendimentoRepository.java
+++ src/main/java/com/sistema_hospitalar/repository/FichaAtendimentoRepository.java
@@ -4,6 +4,7 @@ import com.sistema_hospitalar.domain.FichaAtendimento;
 import com.sistema_hospitalar.domain.enums.StatusAtendimento;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
@@ -14,6 +15,6 @@ public interface FichaAtendimentoRepository extends JpaRepository<FichaAtendimen
 
     List<FichaAtendimento> findByStatusAndAtivaTrue(StatusAtendimento status);
 
-    @Query("SELECT f FROM FichaAtendimento f JOIN f.triagem t WHERE f.status = :status AND f.ativa = true ORDER BY t.prioridade ASC")
-    List<FichaAtendimento> findByStatusOrderByPrioridade(StatusAtendimento status);
+    @Query("SELECT f FROM FichaAtendimento f JOIN f.triagem t WHERE f.status IN :statusList AND f.ativa = true ORDER BY t.prioridade ASC")
+    List<FichaAtendimento> findByStatusInOrderByPrioridade(@Param("statusList") List<StatusAtendimento> statusList);
 }
